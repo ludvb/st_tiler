a037b9f657cba2c56f8e5aeb02fe6826bf904239
Ludvig Bergenstråhle <ludvig.bergenstrahle@scilifelab.se>
2017-06-01 11:49:31 +0200

    Use fixed tile sizes and pad with alpha=0

--- tile.py
+++ tile.py
@@ -4,6 +4,8 @@
 
 import argparse as ap
 
+from enum import Enum
+
 from functools import partial
 
 import logging
@@ -16,10 +18,35 @@ import queue
 
 import threading
 
+import numpy as np
+
 from scipy.misc import imread, imsave, imresize
 
 
+class Corner(Enum):
+    """
+    Enum of the ''corner'' of the tilemap (i.e., where the (0, 0) coordinate
+    is).
+    """
+    SouthWest = 0  # TMS default
+    NorthWest = 1
+    SouthEast = 2
+    NorthEast = 3
+
+class Order(Enum):
+    """
+    Enum of the coordinate order of the tilemap. If stored in `Order.ColMajor`
+    order, coordinates are given as `(c, r)`, where `c` is the column and `r` is
+    the row of the tilemap. Conversely, if set to `Order.RowMajor`, coordinates
+    will be given as `(r, c)`.
+    """
+    ColMajor = 0
+    RowMajor = 1
+
+
 DEF_TILE_SIZE = [256, 256]
+DEF_ORDER = Order.ColMajor
+DEF_CORNER = Corner.SouthWest
 
 DEF_THREADS = 8
 
@@ -128,7 +155,82 @@ def zoom(img):
     """
     return imresize(img, 50)
 
-def gettiles(tile_shape, img):
+def pad(corner, shape, img, fill=None):
+    """
+    Pad image to shape by expanding the image array from a given corner.
+
+    Parameters
+    ----------
+    corner : Corner
+        The corner to expand from.
+    shape : array-like
+        Target shape. Must be a 2-tuple in which each coordinate is greater than
+        or equal to `img[:2]`.
+    img : numpy.ndarray
+        The input image.
+    fill : numpy.ndarray, optional
+        The value to fill the padded pixels with.
+
+    Returns
+    -------
+    padded_img : numpy.ndarray
+        The padded image.
+    """
+    new_shape = list(shape) + list(img.shape[2:])
+    top, left = [r - s for (r, s) in zip(new_shape[:2], img.shape[:2])]
+    if top < 0 or left < 0:
+        raise ValueError('Target shape is smaller than the shape of the image.')
+    bottom, right = 0, 0
+    if corner == Corner.SouthWest or corner == Corner.NorthWest:
+        left, right = right, left
+    if corner == Corner.NorthWest or corner == Corner.NorthEast:
+        top, bottom = bottom, top
+    bottom, right = [s - x for (s, x) in zip(new_shape[:2], (bottom, right))]
+    padded_img = np.zeros(new_shape)
+    padded_img[top:bottom, left:right] = img
+    if fill is not None:
+        if fill.shape != img.shape[2:]:
+            raise ValueError('Fill value has invalid shape.')
+        padded_img[:top, :] = \
+            padded_img[bottom:, :] = \
+            padded_img[:, :left] = \
+            padded_img[:, right:] = \
+            fill
+    return padded_img
+
+def tilecoordinates(order, corner, shape, coordinates):
+    """
+    Translate tile coordinates given in ''image coordinates'' to another basis.
+    If `order == Order.RowMajor` and `corner == Corner.NorthWest`, this is the
+    identity function.
+
+    Parameters
+    ----------
+    order : Order
+        See `Order`.
+    corner : Corner
+        See `Corner`.
+    shape : array_like
+        The shape of the tilemap.
+    coordinates : array_like
+        The coordinates of the tile in ''image coordinates'', i.e., `(row,
+        column)`, where `(0, 0)` represents the north west corner of the image.
+
+    Returns
+    -------
+    new_coordinates : tuple
+        A 2-tuple `(r, c)` where `r` is the current row and `c` is the current
+        column of the tile.
+    """
+    rows, cols = shape
+    row, col = coordinates
+    if corner == Corner.SouthEast or corner == Corner.NorthEast:
+        col = cols - col - 1
+    if corner == Corner.SouthWest or corner == Corner.SouthEast:
+        row = rows - row - 1
+    return (col, row) if order == Order.ColMajor else (row, col)
+
+def gettiles(tile_shape, img, corner=Corner.SouthWest, order=Order.ColMajor):
     """
     Tile image.
 
@@ -138,28 +240,34 @@ def gettiles(tile_shape, img):
         The shape of the tiles.
     img : numpy.ndarray
         The input image.
+    corner : Corner, optional
+        See `Corner`. Defaults to `Corner.SouthWest`.
+    order : Order, optional
+        See `Order`. Defaults to `Order.ColMajor`.
 
     Yields
     ------
     tile : numpy.ndarray
         The output tile.
-    row : tuple
-        A 2-tuple `(x, y)` where `x` is the current row and `y` is the total
-        number of rows in the tiled image.
-    column : tuple
-        A 2-tuple `(x, y)` where `x` is the current column and `y` is the total
-        number of columns in the tiled image.
+    coordinate : tuple
+        A 2-tuple `(r, c)` where `r` is the current row and `c` is the current
+        column.
     """
     rows, cols = [ceil(s / t) for (s, t) in zip(img.shape, tile_shape)]
+    tilecoordinates_ = partial(tilecoordinates, order, corner, (rows, cols))
+    img = pad(
+        corner,
+        [s * n for (s, n) in zip(tile_shape, (rows, cols))],
+        img,
+    )
     for (row, col) in ((r, c) for r in range(rows) for c in range(cols)):
-        yield tuple((
+        yield (
             img[
                 slice(tile_shape[0] * row, tile_shape[0] * (row + 1)),
                 slice(tile_shape[1] * col, tile_shape[1] * (col + 1))
             ],
-            tuple((row, rows)),
-            tuple((col, cols)),
-        ))
+            tilecoordinates_((row, col)),
+        )
 
 def tile(tile_shape, img, nlevels=None):
     """
@@ -187,8 +295,8 @@ def tile(tile_shape, img, nlevels=None):
             for (s, t) in zip(img.shape, tile_shape)
         ])
     for level in reversed(range(nlevels)):
-        for curtile, row, col in gettiles(tile_shape, img):
-            yield curtile, row, col, tuple((level, nlevels))
+        for curtile, (row, col) in gettiles(tile_shape, img):
+            yield curtile, row, col, level
         if level > 0:
             img = zoom(img)
 
@@ -200,30 +308,25 @@ def save(output_dir, row, col, lvl, image_tile):
     ----------
     output_dir : str
         Root output directory.
-    row : tuple
-        A 2-tuple `(x, y)` where `x` is the current row and `y` is the total
-        number of rows in the tiled image.
-    col : tuple
-        A 2-tuple `(x, y)` where `x` is the current column and `y` is the total
-        number of columns in the tiled image.
-    lvl : tuple
-        A 2-tuple `(x, y)` where `x` is the current level and `y` is the total
-        number of levels in the tiled image.
+    maj : int
+        The major order coordinate of the tile. For example, if the tiles are
+        given in `Order.RowMajor` order, this is the row of the tile.
+    min : int
+        The minor order coordinate of the tile.
+    lvl : int
+        The zoom level of the tile.
     image_tile : numpy.ndarray
         The tile to save.
     """
-    currow, maxrows = row
-    curcol, _ = col
-    curlvl, _ = lvl
     dirname = os.path.join(
         output_dir,
-        str(curlvl),
-        str(curcol),
+        str(lvl),
+        str(row),
     )
-    basename = '{:d}.png'.format(maxrows - currow - 1)
+    basename = '{:d}.png'.format(col)
     filename = os.path.join(dirname, basename)
     LOG.info('Writing tile lxrxc={:d}x{:d}x{:d} to {:s}.'.format(
-        curlvl, currow, curcol, filename,
+        lvl, row, col, filename,
     ))
     os.makedirs(dirname, exist_ok=True)
     imsave(filename, image_tile)
@@ -259,7 +362,7 @@ def climain():
     threader = Threader(opt.threads).start()
     for curtile, row, col, lvl in tile(
             opt.shape,
-            imread(opt.input),
+            imread(opt.input, mode='RGBA'),
             opt.levels
     ):
         threader.add(partial(save, opt.output, row, col, lvl, curtile))
